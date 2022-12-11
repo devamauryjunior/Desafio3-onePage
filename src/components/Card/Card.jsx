3bc58aa1b8bf415ac25d1f8f49335221b20227eb
devamauryjunior <jobagpj@gmail.com>
2022-12-11 19:27:51 -0300

    feat: add page main finalizada

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import './style.css';
 
 function Card(props) {
-    const {} = props;
+    const {backgroundColor, color} = props;
+    const myStyleCard = {
+        backgroundColor,
+        color,
+    };
     return (
         <div className="card">
-            <div className="card-color">.</div>
+            <div style={myStyleCard} className="card-color">.</div>
             <h2 className="card-title">Título do card</h2>
             <p className="card-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.
--- src/pages/Main/MainInHtml.jsx
+++ src/pages/Main/MainInHtml.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './style.css';
 import Card from "../../components/Card/Card";
 
 function MainInHtml() {
+    const [arrBackgroundAndColor, setBackgroundAndColor] = useState([]);
+    useEffect(() => {
+        setBackgroundAndColor([
+            {backgroundColor: "#FF6363", color: "#FF6363"},
+            {backgroundColor: "#63ECFF", color: "#63ECFF"},
+            {backgroundColor: "#F363FF", color: "#F363FF"},
+            {backgroundColor: "#63FF73", color: "#63FF73"},
+            {backgroundColor: "#FFDD63", color: "#FFDD63"},
+            {backgroundColor: "#6663FF", color: "#6663FF"},
+        ])
+    }, [])
     return (
         <main className="container-2">
-           <Card />
+            {
+                arrBackgroundAndColor.map((style, idx) => <Card key={idx} backgroundColor={style.backgroundColor} color={style.color}/>)
+            }
         </main>
     )
 }
