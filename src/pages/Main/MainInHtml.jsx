da490bd9c9f7d0cdb5f5daf103ba87f6ed1daae7
devamauryjunior <jobagpj@gmail.com>
2022-12-11 21:28:14 -0300

    feat: melhoras estilos

--- src/pages/Main/MainInHtml.jsx
+++ src/pages/Main/MainInHtml.jsx
@@ -3,17 +3,14 @@ import './style.css';
 import Card from "../../components/Card/Card";
 
 function MainInHtml() {
-    const [arrBackgroundAndColor, setBackgroundAndColor] = useState([]);
-    useEffect(() => {
-        setBackgroundAndColor([
-            {backgroundColor: "#FF6363", color: "#FF6363"},
-            {backgroundColor: "#63ECFF", color: "#63ECFF"},
-            {backgroundColor: "#F363FF", color: "#F363FF"},
-            {backgroundColor: "#63FF73", color: "#63FF73"},
-            {backgroundColor: "#FFDD63", color: "#FFDD63"},
-            {backgroundColor: "#6663FF", color: "#6663FF"},
-        ])
-    }, [])
+    const [arrBackgroundAndColor] = useState([
+        {backgroundColor: "#FF6363", color: "#FF6363"},
+        {backgroundColor: "#63ECFF", color: "#63ECFF"},
+        {backgroundColor: "#F363FF", color: "#F363FF"},
+        {backgroundColor: "#63FF73", color: "#63FF73"},
+        {backgroundColor: "#FFDD63", color: "#FFDD63"},
+        {backgroundColor: "#6663FF", color: "#6663FF"},
+    ]);
     return (
         <main className="container-2">
             {
